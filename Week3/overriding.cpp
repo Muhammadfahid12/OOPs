bdbf67a57ff5beccf467ab77c9e5f7bca75e402f
Muhammad Fahid <ranafahadiirshad@gmail.com>
2023-10-26 08:53:33 -0700

    function overriding

--- Week3/overriding.cpp
+++ Week3/overriding.cpp
@@ -1,5 +1,8 @@
 // difference between overloading and overriding in paper point of view
-
+/*override fuction is one in which there is a same function in both base and derived class and when
+derived class object with that function is called ,derived class function will work,not base class.
+In order to get base class function we will use :: (scope resolution operator)
+*/
 #include <iostream>
 using namespace std;
 
@@ -12,7 +15,7 @@ public:
 
     void setName()
     {
-        cout << "I am SetName Function from parent class";
+        cout << "I am SetName Function from parent class"<<endl;
     }
 };
 class Child:public Person
@@ -21,7 +24,7 @@ class Child:public Person
 public:
         string setName()
         {
-            cout <<"I am setName Function from Child Class";
+            cout <<"I am setName Function from Child Class"<<endl;
             return "A";
         }
 
@@ -31,4 +34,5 @@ int main()
 {
     Child fahid;
     fahid.setName();
+    fahid.Person::setName();
 }
