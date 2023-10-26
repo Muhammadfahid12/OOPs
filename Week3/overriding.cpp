aa777acd7ec1b124fb326ac5862e947456beaf70
Muhammad Fahid <ranafahadiirshad@gmail.com>
2023-10-26 08:37:49 -0700

    -changes

--- Week3/overriding.cpp
+++ Week3/overriding.cpp
@@ -1,2 +1,34 @@
-//difference between overloading and overriding in paper point of view
+// difference between overloading and overriding in paper point of view
 
+#include <iostream>
+using namespace std;
+
+class Person
+{
+    string name;
+    int age;
+
+public:
+
+    void setName()
+    {
+        cout << "I am SetName Function from parent class";
+    }
+};
+class Child:public Person
+{
+
+public:
+        string setName()
+        {
+            cout <<"I am setName Function from Child Class";
+            return "A";
+        }
+
+};
+
+int main()
+{
+    Child fahid;
+    fahid.setName();
+}
