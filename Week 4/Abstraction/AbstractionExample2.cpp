6ce245e47fd88734b78884cb2e48c8921e36c8ae
Muhammad Fahid <ranafahadirshad@gmail.com>
2023-10-27 08:03:47 -0700

    Abstraction Example

---
+++ Week 4/Abstraction/AbstractionExample2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle
+{
+    int engine;
+
+public:
+    void setEngine(int eng)
+    {
+        engine = eng;
+    }
+
+    int getEngine()
+    {
+        return engine;
+    }
+
+    virtual void start() = 0;
+};
+
+class Car : public Vehicle
+{
+    string transmission;
+
+public:
+    void start()
+    {
+        cout << "Start from child class Car";
+    }
+    void setTransmission(string trans)
+    {
+        transmission = trans;
+    }
+    string getTransmission()
+    {
+        return transmission;
+    }
+};
+
+int main()
+{
+    Car audi;
+    audi.setEngine(22);
+    // cout <<audi.getEngine();
+    audi.setTransmission("EBC");
+    cout << audi.getTransmission();
+
+    return 0;
+}
